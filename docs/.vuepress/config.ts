3eb8e3041542430dd7c651e1b2770df4b09f3055
admin@ljj <1918652173@qq.com>
2023-02-11 12:35:55 +0800

    vue build project update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
     //site config
-    base: '/jilijili-music-doc',
+    base: '/jilijili-music-doc/',
     lang: 'zh-CN',
     title: '肌理肌理分布式项目',
     description: '视频,音乐,直播,内容',
